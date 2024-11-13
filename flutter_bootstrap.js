9fa417c06d50cbf89f189dd333232a6fb5ba103c
hoanghdtv <hoanghdtv154@gmail.com>
2024-11-13 15:13:07 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3467476465"
+    serviceWorkerVersion: "1222854492"
   }
 });
